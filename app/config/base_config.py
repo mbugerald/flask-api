d8d559b4da90c3d6af327298422a9f86274f2395
Mbu Gerald <34851885+mbugerald@users.noreply.github.com>
2018-01-03 07:48:16 +0100

    Update base_config.py

--- app/config/base_config.py
+++ app/config/base_config.py
@@ -2,6 +2,6 @@
 import os
 
 
-SQLALCHEMY_DATABASE_URI = 'mysql://gerald:grace1994@localhost/todos'
+SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/todos'
 SECRET_KEY = 'My Todo Application'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
